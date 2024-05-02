f237fe72ed022f6dfaecd17d66a47ac694d79bf5
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-05-02 17:32:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const table_js = document.getElementById('sampleTable');
 function insert_Row() {
-    //document.createElement('tr')//create tr & td like this also works but new way ->
+//document.createElement('tr')//create tr & td like this and then append also works 
+//but new way ->
   const newRow = table_js.insertRow();//0th or -1th idx pe insert kar sakte
   const col1 = newRow.insertCell();
   const col2 = newRow.insertCell();
-	col1.innerText = 'New Cell1';
+	col1.innerText = "New cell1";
 	col2.innerText = "New Cell2";
 }
